c53d3ad762a2c5f95a483560343417abba773bb2
jrosasr <juan.rosasr01@gmail.com>
2025-07-21 13:54:39 -0400

    feat: Add Spanish translations for PWA configuration messages

---
+++ lang/vendor/filament-settings-hub/es/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'group' => 'Configurar PWA',
+    'back' => 'Volver',
+    'save' => 'Guardar',
+    'cancel' => 'Cancelar',
+    'title' => 'PWA',
+    // Agrega aquí otras traducciones necesarias según lo que veas en el panel
+];
